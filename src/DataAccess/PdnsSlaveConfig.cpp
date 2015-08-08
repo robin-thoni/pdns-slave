791b0b8af08f04fa7c95c2730751b1ae5d35afa6
Robin THONI <robin@rthoni.com>
2015-08-08 23:13:35 +0200

    pdns-slave.json data access

---
+++ src/DataAccess/PdnsSlaveConfig.cpp
@@ -0,0 +1,98 @@
+//
+// Created by robin on 8/8/15.
+//
+
+#include <fstream>
+#include <iostream>
+#include "PdnsSlaveConfig.h"
+
+PdnsSlaveConfig::PdnsSlaveConfig(const std::string &filePath)
+    : _filePath(filePath)
+{
+}
+
+bool PdnsSlaveConfig::readConfig()
+{
+    std::ifstream file(_filePath);
+    if (!file)
+        return false;
+    try
+    {
+        Json::Value root;
+        file >> root;
+        file.close();
+        if (!readString(root, "dhcpd-file", _dhcpdFilePath))
+            return false;
+        if (!readString(root, "dhcpd-template", _dhcpdTemplatePath))
+            return false;
+        if (!readString(root, "hosts-file", _hostsPath))
+            return false;
+        if (!readSqlConfiguration(root, "master", _masterConfig))
+            return false;
+        if (!readSqlConfiguration(root, "slave", _slaveConfig))
+            return false;
+    }
+    catch (...)
+    {
+        file.close();
+        return false;
+    }
+
+    return true;
+}
+
+bool PdnsSlaveConfig::readSqlConfiguration(const Json::Value &value, const std::string &name, SqlConfiguration &sqlConfiguration)
+{
+    std::string str;
+    if (!readString(value, name + "-host", str))
+        return false;
+    sqlConfiguration.setHost(str);
+
+    if (!readString(value, name + "-user", str))
+        return false;
+    sqlConfiguration.setUser(str);
+
+    if (!readString(value, name + "-password", str))
+        return false;
+    sqlConfiguration.setPassword(str);
+
+    if (!readString(value, name + "-database", str))
+        return false;
+    sqlConfiguration.setDatabase(str);
+
+    return true;
+}
+
+bool PdnsSlaveConfig::readString(const Json::Value &value, const std::string &name, std::string &dest)
+{
+    auto v = value[name];
+    if (!v)
+        return false;
+    dest = v.asString();
+    return true;
+}
+
+const std::string &PdnsSlaveConfig::getDhcpdFilePath() const
+{
+    return _dhcpdFilePath;
+}
+
+const std::string &PdnsSlaveConfig::getDhcpdTemplatePath() const
+{
+    return _dhcpdTemplatePath;
+}
+
+const std::string &PdnsSlaveConfig::getHostsPath() const
+{
+    return _hostsPath;
+}
+
+const SqlConfiguration &PdnsSlaveConfig::getMasterConfig() const
+{
+    return _masterConfig;
+}
+
+const SqlConfiguration &PdnsSlaveConfig::getSlaveConfig() const
+{
+    return _slaveConfig;
+}
---
+++ src/DataAccess/PdnsSlaveConfig.h
@@ -0,0 +1,49 @@
+//
+// Created by robin on 8/8/15.
+//
+
+#ifndef PDNS_SLAVE_PDNSSLAVECONFIG_H
+#define PDNS_SLAVE_PDNSSLAVECONFIG_H
+
+# include <string>
+# include <json/json.h>
+# include "DBO/SqlConfiguration.h"
+
+class PdnsSlaveConfig
+{
+public:
+    PdnsSlaveConfig(const std::string& filePath);
+
+    bool readConfig();
+
+    const std::string &getDhcpdFilePath() const;
+
+    const std::string &getDhcpdTemplatePath() const;
+
+    const std::string &getHostsPath() const;
+
+    const SqlConfiguration &getMasterConfig() const;
+
+    const SqlConfiguration &getSlaveConfig() const;
+
+private:
+    bool readString(const Json::Value& value, const std::string& name, std::string& dest);
+
+    bool readSqlConfiguration(const Json::Value &value, const std::string &name, SqlConfiguration& sqlConfiguration);
+
+    std::string _filePath;
+
+private:
+    std::string _dhcpdFilePath;
+
+    std::string _dhcpdTemplatePath;
+
+    std::string _hostsPath;
+
+    SqlConfiguration _masterConfig;
+
+    SqlConfiguration _slaveConfig;
+};
+
+
+#endif //PDNS_SLAVE_PDNSSLAVECONFIG_H
